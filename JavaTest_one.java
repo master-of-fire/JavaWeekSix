f8694dc6460892ea0d9546e3b9e14a62150bc46d
master-of-fire <99187679+master-of-fire@users.noreply.github.com>
2025-03-10 10:30:01 -0400

    .

---
+++ JavaTest_one.java
@@ -0,0 +1,19 @@
+// Name: Zaid Ahmad
+// Date: March 10th
+// App name: JavaTest_one.java
+
+
+public class JavaTest_one {
+    public static void main(String[]args) {
+
+    double volume;
+    double cubicYards;
+    double concreteCost;
+    volume = 3.14*(3*3)*5;
+    System.out.println(volume);
+    cubicYards = volume/27;
+    System.out.println("there are "+cubicYards+" cubic yards");
+    concreteCost = cubicYards*12.98;
+    System.out.println("the cost of the concrete is $"+concreteCost);
+    }
+}
---
+++ JavaTest_two.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+// Name: Zaid Ahmad
+// Date: March 10th
+// App name: JavaTest_two.java
+
+
+public class JavaTest_two {
+    public static void main(String[]args) {
+    double m;
+    double n;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the longer side of the right triangle: ");
+    m = input.nextDouble();
+    
+    Scanner input1 = new Scanner(System.in);
+    System.out.println("Enter the shorter side of the right triangle: ");
+    n = input1.nextDouble();
+    }
+}
