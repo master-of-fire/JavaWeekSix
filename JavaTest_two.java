f225338c54ebe48323a6d7cca44c620c1421c468
master-of-fire <99187679+master-of-fire@users.noreply.github.com>
2025-03-18 10:19:05 -0400

    Update JavaTest_two.java

--- JavaTest_two.java
+++ JavaTest_two.java
@@ -9,12 +9,20 @@ public class JavaTest_two {
     public static void main(String[]args) {
     double m;
     double n;
+    double SideOne;
+    double SideTwo;
+    double Hypotenuse;
+    double cost;
     Scanner input = new Scanner(System.in);
     System.out.println("Enter the longer side of the right triangle: ");
     m = input.nextDouble();
-    
     Scanner input1 = new Scanner(System.in);
     System.out.println("Enter the shorter side of the right triangle: ");
     n = input1.nextDouble();
+    SideOne = (m*m)-(n*n);
+    SideTwo = 2*m*n;
+    Hypotenuse = (m*m)+(n*n);
+    cost = 2.412*SideOne+3.767*SideTwo+15.758*Hypotenuse;
+    System.out.println("The Cost is " cost);
     }
 }
